01d44901ce792e7662d313679f56f3338de0abbb
slightlybae <45470819+slightlybae@users.noreply.github.com>
2020-06-14 23:27:42 +0100

    Add files via upload

---
+++ Word Translator/Word Translator/src/word Translator.kt
@@ -0,0 +1,19 @@
+
+/**
+   This is a word Translator
+   English to Igbo & Yoruba language
+**/
+
+
+
+
+fun main(args: Array<String>) {
+     var english = arrayOf<String>( "Good Morning", "Goodafternoon", "Goodnight")
+     var igbo = arrayOf<String>( "Ututu Oma", "EhiheOma", "Kachifo")
+     var yoruba = arrayOf<String>( "E Kaaro", "E Kaasan", "E kaale")
+
+   println(english.get(0))
+   println(igbo.get(0))
+    println(yoruba.get(0))
+    //println(english.indexOf("Goodmorning"))
+}
